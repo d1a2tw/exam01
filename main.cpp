c6f18737d84d402239c037b0b0f636da2d05d09a
d1a2tw <d1a2tw@gmail.com>
2020-04-01 02:48:47 +0800

    123

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include "mbed.h"
 
-
+#include "mbed.h"
+#include "uLCD_4DGL.h"
 PwmOut PWM1(D6);
+uLCD_4DGL uLCD(D1, D0, D2);
 AnalogIn Ain(D0);
 float data; 
-
+int sample =128;
 int main() {
 
 while(1)
 {
+ uLCD.printf("\n105061151\n");
+uLCD.rectangle(1,1,1,1,WHITE);
+
+
 
   PWM1.period(0.001);
   PWM1=0;
@@ -20,8 +26,7 @@ for(int i=0;i<20 ;i++)
 
 data=Ain;
 wait(0.1);
-pc.printf("%1.3f\r\n", ADCdata);
-
+pc.printf("%1.3f\r\n", data);
 
 
 
